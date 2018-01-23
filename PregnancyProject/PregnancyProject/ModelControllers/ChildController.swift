ee7fb87558c56ee6cdeab338adf6f85ca78b25b5
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MBP.hsd1.ut.comcast.net>
2018-01-22 17:31:12 -0700

    create model controllers for model objects

---
+++ PregnancyProject/PregnancyProject/ModelControllers/ChildController.swift
@@ -0,0 +1,14 @@
+//
+//  ChildController.swift
+//  PregnancyProject
+//
+//  Created by Kaitlyn Barker on 1/22/18.
+//  Copyright © 2018 Kaitlyn Barker. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class ChildController {
+    
+}
---
+++ PregnancyProject/PregnancyProject/ModelControllers/EntryController.swift
@@ -0,0 +1,14 @@
+//
+//  EntryController.swift
+//  PregnancyProject
+//
+//  Created by Kaitlyn Barker on 1/22/18.
+//  Copyright © 2018 Kaitlyn Barker. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class EntryController {
+    
+}
