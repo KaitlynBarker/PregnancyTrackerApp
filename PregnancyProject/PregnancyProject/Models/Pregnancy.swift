4f589c35dbd27b3aaf218b81725499ae961f9e89
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MBP.hsd1.ut.comcast.net>
2018-01-23 22:58:18 -0700

    fill out pregnancy MO

--- PregnancyProject/PregnancyProject/Models/Pregnancy.swift
+++ PregnancyProject/PregnancyProject/Models/Pregnancy.swift
@@ -10,5 +10,47 @@ import Foundation
 import CloudKit
 
 class Pregnancy {
+    // possibly have a badge array
     
+    fileprivate static var nameKey: String { return "name" }
+    fileprivate static var dueDateKey: String { return "dueDate" }
+    fileprivate static var entryRefsKey: String { return "entryRefs" }
+    static var recordType: String { return "Pregnancy" }
+    
+    let name: String
+    let dueDate: Date
+    let entryRefs: [CKReference]
+    
+    var ckRecordID: CKRecordID?
+    
+    init(name: String, dueDate: Date, entryRefs: [CKReference] = []) {
+        self.name = name
+        self.dueDate = dueDate
+        self.entryRefs = entryRefs
+    }
+    
+    init?(ckRecord: CKRecord) {
+        guard let name = ckRecord[Pregnancy.nameKey] as? String,
+            let dueDate = ckRecord[Pregnancy.dueDateKey] as? Date else { return nil }
+        
+        self.name = name
+        self.dueDate = dueDate
+        self.entryRefs = ckRecord[Pregnancy.entryRefsKey] as? [CKReference] ?? []
+    }
+}
+
+extension CKRecord {
+    convenience init(pregnancy: Pregnancy) {
+        let recordID = pregnancy.ckRecordID ?? CKRecordID(recordName: UUID().uuidString)
+        
+        self.init(recordType: Pregnancy.recordType, recordID: recordID)
+        
+        if pregnancy.entryRefs == [] {
+        } else {
+            self.setValue(pregnancy.entryRefs, forKey: Pregnancy.entryRefsKey)
+        }
+        
+        self.setValue(pregnancy.name, forKey: Pregnancy.nameKey)
+        self.setValue(pregnancy.dueDate, forKey: Pregnancy.dueDateKey)
+    }
 }
