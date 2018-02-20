834328a3127e583b0ffe754557a17fee88ee076f
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MBP.hsd1.ut.comcast.net>
2018-02-20 12:31:09 -0700

    add badge reference array to pregnancy MO

--- PregnancyProject/PregnancyProject/Models/Pregnancy.swift
+++ PregnancyProject/PregnancyProject/Models/Pregnancy.swift
@@ -10,23 +10,25 @@ import Foundation
 import CloudKit
 
 class Pregnancy {
-    // possibly have a badge array
     
     fileprivate static var nameKey: String { return "name" }
     fileprivate static var dueDateKey: String { return "dueDate" }
     fileprivate static var entryRefsKey: String { return "entryRefs" }
+    fileprivate static var badgeRefsKey: String { return "badgeRefs" }
     static var recordType: String { return "Pregnancy" }
     
     let name: String
     let dueDate: Date
     let entryRefs: [CKReference]
+    let badgeRefs: [CKReference]
     
     var ckRecordID: CKRecordID?
     
-    init(name: String, dueDate: Date, entryRefs: [CKReference] = []) {
+    init(name: String, dueDate: Date, entryRefs: [CKReference] = [], badgeRefs: [CKReference] = []) {
         self.name = name
         self.dueDate = dueDate
         self.entryRefs = entryRefs
+        self.badgeRefs = badgeRefs
     }
     
     init?(ckRecord: CKRecord) {
@@ -36,12 +38,13 @@ class Pregnancy {
         self.name = name
         self.dueDate = dueDate
         self.entryRefs = ckRecord[Pregnancy.entryRefsKey] as? [CKReference] ?? []
+        self.badgeRefs = ckRecord[Pregnancy.badgeRefsKey] as? [CKReference] ?? []
     }
 }
 
 extension Pregnancy: Equatable {
     static func ==(lhs: Pregnancy, rhs: Pregnancy) -> Bool {
-        return lhs.name == rhs.name && lhs.dueDate == rhs.dueDate && lhs.entryRefs == rhs.entryRefs
+        return lhs.name == rhs.name && lhs.dueDate == rhs.dueDate && lhs.entryRefs == rhs.entryRefs && lhs.badgeRefs == rhs.badgeRefs
     }
 }
 
@@ -54,6 +57,11 @@ extension CKRecord {
         if pregnancy.entryRefs == [] {
         } else {
             self.setValue(pregnancy.entryRefs, forKey: Pregnancy.entryRefsKey)
+        }
+        
+        if pregnancy.badgeRefs == [] {
+        } else {
+            self.setValue(pregnancy.badgeRefs, forKey: Pregnancy.badgeRefsKey)
         }
         
         self.setValue(pregnancy.name, forKey: Pregnancy.nameKey)
