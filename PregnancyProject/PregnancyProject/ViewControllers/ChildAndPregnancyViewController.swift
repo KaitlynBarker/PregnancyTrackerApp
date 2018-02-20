9596673493dc5d6c4f6aaee3db27d94bac8a627a
Kaitlyn Barker <kaitlynbarker@Kaitlyns-MBP.hsd1.ut.comcast.net>
2018-02-20 15:09:18 -0700

    create main screen VC and set constraints

---
+++ PregnancyProject/PregnancyProject/ViewControllers/ChildAndPregnancyViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ChildAndPregnancyViewController.swift
+//  PregnancyProject
+//
+//  Created by Kaitlyn Barker on 2/20/18.
+//  Copyright © 2018 Kaitlyn Barker. All rights reserved.
+//
+
+import UIKit
+
+class ChildAndPregnancyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
